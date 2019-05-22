79a7b7e1c77cb004df27573b081c872408092d17
Juan Martin Leiva <jm.leiva91@gmail.com>
2019-05-22 19:39:12 -0300

    Actualizada version de Android Studio + Agregadas dependencias comunes

--- app/src/main/java/com/jml/rappichallenge/MainActivity.java
+++ app/src/main/java/com/jml/rappichallenge/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jml.rappichallenge;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
