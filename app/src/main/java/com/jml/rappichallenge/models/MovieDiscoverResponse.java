030be47566cbc1dbec7a15f2c8e98eb95f0ac4af
Juan Martin Leiva <jm.leiva91@gmail.com>
2019-05-22 20:54:22 -0300

    DTOs y Modelos creados

---
+++ app/src/main/java/com/jml/rappichallenge/models/MovieDiscoverResponse.java
@@ -0,0 +1,58 @@
+package com.jml.rappichallenge.models;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieDiscoverResponse
+{
+	private static class DTO
+	{
+		private int page;
+		private int total_results;
+		private int total_pages;
+		private List<Movie> results;
+	}
+
+	private DTO dto;
+
+	MovieDiscoverResponse(DTO dto)
+	{
+		this.dto = dto;
+	}
+
+	public int getPage()
+	{
+		return dto.page;
+	}
+
+	public int getTotalResultCount()
+	{
+		return dto.total_results;
+	}
+
+	public int getPagesCount()
+	{
+		return dto.total_pages;
+	}
+
+	public List<Movie> getResult()
+	{
+		return new ArrayList<>(dto.results);
+	}
+
+	public static class Deserializer implements JsonDeserializer<MovieDiscoverResponse>
+	{
+		@Override
+		public MovieDiscoverResponse deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
+		{
+			MovieDiscoverResponse.DTO dto = context.deserialize(json, MovieDiscoverResponse.DTO.class);
+			return new MovieDiscoverResponse(dto);
+		}
+	}
+}
